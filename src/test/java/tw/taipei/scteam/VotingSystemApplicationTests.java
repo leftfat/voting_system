95f82ce65d26d648b4347006d58281d496adcac5
leftfat <151799245+leftfat@users.noreply.github.com>
2024-03-16 09:37:51 +0800

    init

---
+++ src/test/java/tw/taipei/scteam/VotingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.taipei.scteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
